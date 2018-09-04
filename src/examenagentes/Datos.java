1adbad53ac4a72329f2c9f017a0aec04b17641cd
Jose Manuel Paredes Ramirez <ironglas68@gmail.com>
2018-09-03 23:28:32 -0500

    Examen de programación. Ordenar 3 números de manera ascendente y descendente

---
+++ src/examenagentes/Datos.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examenagentes;
+
+/**
+ *
+ * @author Pepe
+ */
+public class Datos {
+     int mb1 = 0;
+     int mb2 = 0;
+     int mb3 = 0;
+     
+     
+     
+    
+}
